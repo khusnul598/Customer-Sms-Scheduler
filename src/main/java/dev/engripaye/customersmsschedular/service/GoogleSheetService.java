83037a12d441c5e37f6f5e46c6ef19afa66d8dd5
Ipaye Babatunde <fineboyhakim@gmail.com>
2025-11-15 18:43:55 +0100

    backend code commit by ENGR. IPAYE

--- src/main/java/dev/engripaye/customersmsschedular/service/GoogleSheetService.java
+++ src/main/java/dev/engripaye/customersmsschedular/service/GoogleSheetService.java
@@ -1,9 +1,47 @@
 package dev.engripaye.customersmsschedular.service;
 
+import com.google.api.client.googleapis.javanet.GoogleNetHttpTransport;
+import com.google.api.client.json.jackson2.JacksonFactory;
+import com.google.api.services.sheets.v4.Sheets;
+import com.google.api.services.sheets.v4.model.ValueRange;
+import com.google.auth.http.HttpCredentialsAdapter;
+import com.google.auth.oauth2.ServiceAccountCredentials;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
+
+import java.io.FileInputStream;
+import java.util.List;
 
 @Service
 public class GoogleSheetService {
 
+    @Value("${google.sheet.id}")
+    private String spreadsheetId;
 
+    @Value("${google.sheet.range}")
+    private String range;
+
+    @Value("${google.credentials.file.path}")
+    private String serviceAccountPath;
+
+
+    public List<List<Object>> getAllCustomers() throws Exception {
+        FileInputStream serviceAccountStream = new FileInputStream(serviceAccountPath);
+
+        ServiceAccountCredentials credentials =
+                ServiceAccountCredentials.fromStream(serviceAccountStream);
+
+        Sheets sheetsService = new Sheets.Builder(
+                GoogleNetHttpTransport.newTrustedTransport(),
+                JacksonFactory.getDefaultInstance(),
+                new HttpCredentialsAdapter(credentials)
+        ).setApplicationName("Customer SMS Scheduler")
+                .build();
+
+        ValueRange response = sheetsService.spreadsheets().values()
+                .get(spreadsheetId, range)
+                .execute();
+
+        return response.getValues();
+    }
 }
