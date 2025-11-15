b75a6626e9b12b087c53088ae6963041182121f6
Ipaye Babatunde <fineboyhakim@gmail.com>
2025-11-15 17:52:27 +0100

    init commit

---
+++ src/main/java/dev/engripaye/customersmsschedular/CustomerSmsSchedularApplication.java
@@ -0,0 +1,13 @@
+package dev.engripaye.customersmsschedular;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CustomerSmsSchedularApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CustomerSmsSchedularApplication.class, args);
+    }
+
+}
